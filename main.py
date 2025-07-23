1630a6827f958068b928003005cd56c33432ca83
Oliviaheckert <107218530+Oliviaheckert@users.noreply.github.com>
2025-07-23 16:27:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from exa_py import Exa
 
 exa = Exa('API_KEY')
 
-// holds repsonse to the input of what we want to search
+# holds repsonse to the input of what we want to search
 query = input('Search here: ')
 
 response = exa.search(
-	'Best New York bagel',
-    num_results=10,
+	query,
+  num_results=10,
+  type='keyword',
+  include_domains=['https://www.tiktok.com'],
 )
+
+for result in response.results:
+  print(f'Title: {result.title}')
+  print(f'URL: {result.url}')
+  print()
